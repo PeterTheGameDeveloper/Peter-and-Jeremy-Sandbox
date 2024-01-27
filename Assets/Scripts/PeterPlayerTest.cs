001c4ddb361132ebdc31bf353cdb79fb9bc361e8
PeterTheGameDeveloper <peterkline82@gmail.com>
2024-01-27 10:52:55 -0700

    Made it to where if you are moving to the left it flips the sprite

--- Assets/Scripts/PeterPlayerTest.cs
+++ Assets/Scripts/PeterPlayerTest.cs
@@ -19,16 +19,25 @@ public class PeterPlayerTest : MonoBehaviour
     {
         playerAnimator.SetBool("IsRunning", false);
         Vector3 currentPlayerPosition = transform.position;
+        Vector3 currentPlayerScale = transform.localScale;
 
         if (Input.GetKey(KeyCode.A))
         {
             currentPlayerPosition.x = transform.position.x - xMoveSpeed;
             playerAnimator.SetBool("IsRunning", true);
+            if (currentPlayerScale.x >= 0)
+            {
+                transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+            }
         }
         if (Input.GetKey(KeyCode.D))
         {
             currentPlayerPosition.x = transform.position.x + xMoveSpeed;
             playerAnimator.SetBool("IsRunning", true);
+            if (currentPlayerScale.x < 0)
+            {
+                transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+            }
         }
         if (Input.GetKey(KeyCode.S))
         {
