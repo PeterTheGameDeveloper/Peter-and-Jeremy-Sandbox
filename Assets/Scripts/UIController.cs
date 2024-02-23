bbbb4e94e620189de29711c81a1b1feebed5ac19
PeterTheGameDeveloper <peterkline82@gmail.com>
2024-02-23 12:07:51 -0700

    UI improvements and added item pickup

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -1,12 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIController : MonoBehaviour
 {
-    public Animator uiAnimator;
-    public void Inventory(bool inventoryOpen)
+    public GameObject inventory;
+    public GameObject armor;
+    public GameObject stats;
+
+    private void Awake()
     {
-        uiAnimator.SetBool("InventoryOpen", inventoryOpen);
+        inventory.SetActive(false);
+        armor.SetActive(false);
+        stats.SetActive(false);
+    }
+
+    public void UIViewer(string targetObjectName)
+    {
+        GameObject targetObject = null;
+        List<GameObject> uiObjects = new() { armor, inventory, stats };
+        foreach (GameObject item in uiObjects)
+        {
+            if (item.name == targetObjectName)
+            {
+                targetObject = item;
+                break;
+            }
+        }
+        if (targetObject != null)
+        {
+            uiObjects.Remove(targetObject);
+            foreach (GameObject uiObject in uiObjects)
+            {
+                uiObject.SetActive(false);
+            }
+            if (targetObject.activeSelf)
+                targetObject.SetActive(false);
+            else
+                targetObject.SetActive(true);
+        }
     }
 }
